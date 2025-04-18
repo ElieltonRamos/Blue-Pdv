2b1423ae4d486b817dcfc600523f6d1bce889d35
Elielton <elieltonramos14@gmail.com>
2025-04-17 23:03:55 -0300

    feat(frontend): :art: atualizanod estilo da pagina de venda

--- frontend/src/app/pages/vendas/vendas.component.ts
+++ frontend/src/app/pages/vendas/vendas.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vendas',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './vendas.component.html',
 })
 export class VendasComponent {
+
+  client = { id:1, name: 'Elielton' }
+  vendedorLogado = 'Elielton'
+  codigoBarras: string = '';
+
+buscarProdutoPorCodigo() {
+  console.log('Produto escaneado:', this.codigoBarras);
+  // aqui você pode buscar o produto no banco ou array local
+  this.codigoBarras = ''; // limpa o campo após leitura
+}
+
 
 }
