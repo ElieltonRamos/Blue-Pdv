a9bb83e205de686beb147a37398702707ab7c2be
Elielton <elieltonramos14@gmail.com>
2025-04-18 17:48:58 -0300

    adiciona configuração do banco de dados e modelos de usuário ao backend

---
+++ backend/src/database/migrations/01-create-users.ts
@@ -0,0 +1,30 @@
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import User from '../../interfaces/user';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<User>>('users', {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      function: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      }
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('users');
+  }
+};
---
+++ backend/src/database/seeders/1-users.ts
@@ -0,0 +1,32 @@
+import { QueryInterface } from 'sequelize';
+import bcrypt from 'bcryptjs';
+import { password } from '../config/database';
+
+const SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS || 10;
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          id: 1,
+          username: 'Elielton',
+          password: bcrypt.hashSync('123456', SALT_ROUNDS),
+          function: 'admin',
+        },
+        {
+          id: 2,
+          username: 'Lucas',
+          password:bcrypt.hashSync('123456', SALT_ROUNDS),
+          function: 'user',
+        }
+      ],
+      {}
+    );
+  },
+
+  down(queryInterface: QueryInterface) {
+    return queryInterface.bulkDelete('users', {});
+  },
+};
