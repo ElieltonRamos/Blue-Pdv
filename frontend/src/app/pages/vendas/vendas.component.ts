999bf1eb9938c960b2c6a99d7a39bdd191263a3c
Elielton <elieltonramos14@gmail.com>
2025-04-22 22:04:11 -0300

    feat(frontend): :sparkles: adicionar interface Product e implementar exibição de produtos na página de vendas

--- frontend/src/app/pages/vendas/vendas.component.ts
+++ frontend/src/app/pages/vendas/vendas.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ClientService } from '../../services/client.service';
 import Client from '../../interfaces/client';
+import Product from '../../interfaces/product';
 
 @Component({
   selector: 'app-vendas',
@@ -9,19 +10,30 @@ import Client from '../../interfaces/client';
   templateUrl: './vendas.component.html',
 })
 export class VendasComponent {
-
-  client: Client = { id:1, name: 'Avista', phone: '0', adress: 'rua 0' }
+  client: Client = { id: 1, name: 'Avista', phone: '0', adress: 'rua 0' };
   token = JSON.parse(localStorage.getItem('token') || '');
   vendedorLogado = this.token.token.username;
   codigoBarras: string = '';
-  searchClients: Client[] = []
+  searchClients: Client[] = [];
+  products: Product[] = [
+    { id: 1, name: 'Produto 1', price: 10, code: '1', quantity: 1 },
+    { id: 2, name: 'Produto 2', price: 20, code: '2', quantity: 2 },
+  ];
 
-  constructor(private clientService: ClientService) {};
+  constructor(private clientService: ClientService) {}
 
   buscarProdutoPorCodigo() {
     console.log('Produto escaneado:', this.codigoBarras);
     // aqui você pode buscar o produto no banco ou array local
     this.codigoBarras = ''; // limpa o campo após leitura
+  }
+
+  removeItem(item: Product) {
+    // lógica pra remover
+  }
+
+  editItem(item: Product) {
+    // lógica pra editar
   }
 
   findClientById(id: string) {
@@ -32,9 +44,9 @@ export class VendasComponent {
       error: (error) => {
         alert('Cliente não encontrado');
         console.log('Erro ao buscar cliente:', error);
-      }
+      },
     });
-  };
+  }
 
   findClientByName(name: string) {
     return this.clientService.findClientByName(name).subscribe({
@@ -44,8 +56,7 @@ export class VendasComponent {
       error: (error) => {
         alert('Cliente não encontrado');
         console.log('Erro ao buscar cliente:', error);
-      }
+      },
     });
-  };
-
+  }
 }
