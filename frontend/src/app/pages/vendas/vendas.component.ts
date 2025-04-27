e945ef28ea987b3732c1938594916fc1958caf9e
Elielton <elieltonramos14@gmail.com>
2025-04-26 22:32:16 -0300

    feat(frontend): :sparkles: adicionar modal de edição de produto e atualizar lógica de subtotal na página de vendas

--- frontend/src/app/pages/vendas/vendas.component.ts
+++ frontend/src/app/pages/vendas/vendas.component.ts
@@ -4,49 +4,95 @@ import { ClientService } from '../../services/client.service';
 import Client from '../../interfaces/client';
 import Product from '../../interfaces/product';
 import { ProductsService } from '../../services/products.service';
+import { ModalEditProductComponent } from '../../components/modal-edit-product/modal-edit-product.component';
 
 @Component({
   selector: 'app-vendas',
-  imports: [FormsModule],
+  imports: [FormsModule, ModalEditProductComponent],
   templateUrl: './vendas.component.html',
 })
 export class VendasComponent {
   client: Client = { id: 1, name: 'Avista', phone: '0', adress: 'rua 0' };
   token = JSON.parse(localStorage.getItem('token') || '');
   vendedorLogado = this.token.token.username;
-  product = { code: '', quantity: 0, price: 0 };
+  product = { code: '', quantity: 1, price: 0 };
   searchClients: Client[] = [];
-  products: Product[] = [
-    { id: 1, name: 'Produto 1', price: 10, code: '1', quantity: 1 },
-    { id: 2, name: 'Produto 2', price: 20, code: '2', quantity: 2 },
-  ];
+  products: Product[] = [];
+  subtotalValue: number = 0;
+  showEditModal = false;
+  selectedItem!: Product;
 
-  constructor(private clientService: ClientService, private productService: ProductsService) {}
+  constructor(
+    private clientService: ClientService,
+    private productService: ProductsService
+  ) {}
 
-  buscarProdutoPorCodigo() {
+  updateSubtotalValue() {
+    this.subtotalValue = this.products.reduce(
+      (acc, product) => acc + product.price * (product.quantity ?? 1),
+      0
+    );
+  }
+
+  searchProductByCode() {
     if (!this.product.code || this.product.quantity <= 0) {
       return alert('Informe o código e quantidade válidos!');
     }
+
     this.productService.getProductByCode(this.product.code).subscribe({
-      next: (product) => this.products.push({...product, quantity: this.product.quantity}),
+      next: (product) => {
+        const existingProduct = this.products.find(
+          (p) => p.code === product.code
+        );
+        if (existingProduct) {
+          existingProduct.quantity! += this.product.quantity;
+        } else {
+          const newProduct = {
+            ...product,
+            quantity: this.product.quantity,
+            price: product.price,
+          };
+          this.products.push(newProduct);
+        }
+        this.updateSubtotalValue();
+        this.product = { code: '', quantity: 1, price: 0 };
+      },
       error: (error) => alert(error),
     });
-    this.product = { code: '', quantity: 0, price: 0 };
   }
 
   removeItem(item: Product) {
-    const filteredProducts = this.products.filter((product) => product.id !== item.id);
-    this.products = filteredProducts;
+    this.products = this.products.filter((product) => product.id !== item.id);
+    this.updateSubtotalValue();
   }
 
   editItem(item: Product) {
-    const quantity = item.quantity ?? 1;
-    this.product = {...item, quantity};
-    this.removeItem(item);
+    this.selectedItem = { ...item };
+    this.showEditModal = true;
   }
 
-  subtotal(): number {
-    return this.products.reduce((acc, p) => acc + p.price * (p.quantity ?? 1), 0);
+  openEditModal(item: Product) {
+    this.selectedItem = item;
+    this.showEditModal = true;
+  }
+
+  onSaveItem(updatedItem: Product) {
+    const index = this.products.findIndex((p) => p.id === updatedItem.id);
+
+    if (index !== -1) {
+      this.products[index] = updatedItem;
+      this.updateSubtotalValue();
+    }
+
+    this.showEditModal = false;
+  }
+
+  onCancelEdit() {
+    this.showEditModal = false;
+  }
+
+  formatNumber(n: number): number {
+    return parseFloat(n.toFixed(2));
   }
 
   findClientById(id: string) {
@@ -72,5 +118,4 @@ export class VendasComponent {
       },
     });
   }
-
 }
