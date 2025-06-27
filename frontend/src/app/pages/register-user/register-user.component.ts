7a308ab9c4e378487fc573f57f0f9ae7d4c1f2f0
Elielton <elieltonramos14@gmail.com>
2025-06-27 08:01:07 -0300

    feat(frontend): implementar navegação para o menu na tela de registro de usuário

--- frontend/src/app/pages/register-user/register-user.component.ts
+++ frontend/src/app/pages/register-user/register-user.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CreateUserComponent } from '../../components/create-user/create-user.component';
 import { ListUsersComponent } from '../../components/list-users/list-users.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-user',
@@ -8,11 +9,16 @@ import { ListUsersComponent } from '../../components/list-users/list-users.compo
   templateUrl: './register-user.component.html',
 })
 export class RegisterUserComponent {
+  private router = inject(Router)
   menuContext = 'create';
   classBtnSelected = 'bg-sky-500 hover:bg-sky-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 font-semibold';
   classBtnUnselected = 'text-white hover:underline px-4 py-2 rounded-lg flex items-center gap-2 font-semibold';
 
   changeMenuContext(context: string): void {
     this.menuContext = context;
+  }
+
+  goToMenu() {
+    this.router.navigate(['/menu'])
   }
 }
