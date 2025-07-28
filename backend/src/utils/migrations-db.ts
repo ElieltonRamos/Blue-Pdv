0501bbd08c5494293c5a991df361ce1eae621796
Elielton <elieltonramos14@gmail.com>
2025-07-28 11:04:56 -0300

    feat: :sparkles: adiciona scripts de migração e redefinição de banco de dados, além de arquivos executáveis para Windows e Linux

---
+++ backend/src/utils/migrations-db.ts
@@ -0,0 +1,13 @@
+import { exec, ExecException } from 'child_process';
+
+exec('npm run db:migrate', (error: ExecException | null, stdout: string, stderr: string) => {
+  if (error) {
+    console.error(`Erro ao executar o comando: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Erro: ${stderr}`);
+    return;
+  }
+  console.log(`Saída: ${stdout}`);
+});
